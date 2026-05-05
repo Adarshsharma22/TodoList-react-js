ed82b82e6062cefdab0088622a7ba20120fcd8d8
Adarshsharma22 <adarshsharma6222@gmail.com>
2026-05-05 13:27:50 +0530

    Updated bg image

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import './index.css'
 
 function App() {
   return (
-    <div className="bg-[url('./GoodEvening.jpg')] bg-fixed bg-cover bg-center min-h-screen" >
+    <div 
+    className="bg-fixed bg-cover bg-center min-h-screen"
+    style={{
+    backgroundImage: "url('/GoodEvening.jpg')"
+  }} >
       <TodoWrapperLocalStorage  />
     </div>
   );
